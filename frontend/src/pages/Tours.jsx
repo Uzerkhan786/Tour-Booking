ffa688f00d08accfdfa5fb3c30257868d0bdb1d4
Uzerkhan786 <uzer.khan2020@vitbhopal.ac.in>
2024-01-13 11:24:34 +0530

    added changes

--- frontend/src/pages/Tours.jsx
+++ frontend/src/pages/Tours.jsx
@@ -37,25 +37,27 @@ const Tours = () => {
 
          <section className='pt-0'>
             <Container>
-              
-             
-               {!load?(<div ><Loading/></div>):(<Row>
-                     {
-                        tours?.map(tour => (<Col lg='3' md='6' sm='6' className='mb-4' key={tour._id}> <TourCard tour={tour} /> </Col>))
-                     }
-
-                     <Col lg='12'>
-                        <div className="pagination d-flex align-items-center justify-content-center mt-4 gap-3">
-                           {[...Array(pageCount).keys()].map(number => (
-                              <span key={number} onClick={() => setPage(number)}
-                                 className={page === number ? 'active__page' : ''}
-                              >
-                                 {number + 1}
-                              </span>
-                           ))}
-                        </div>
-                     </Col>
-                  </Row>)
+               
+               {error && <h4 className='text-center pt-5'>{error}</h4>}
+               {
+                  loading?(<Loading/>):(!loading && !error &&
+                     <Row>
+                        {
+                           tours?.map(tour => (<Col lg='3' md='6' sm='6' className='mb-4' key={tour._id}> <TourCard tour={tour} /> </Col>))
+                        }
+   
+                        <Col lg='12'>
+                           <div className="pagination d-flex align-items-center justify-content-center mt-4 gap-3">
+                              {[...Array(pageCount).keys()].map(number => (
+                                 <span key={number} onClick={() => setPage(number)}
+                                    className={page === number ? 'active__page' : ''}
+                                 >
+                                    {number + 1}
+                                 </span>
+                              ))}
+                           </div>
+                        </Col>
+                     </Row>)
                   
                }
             </Container>
